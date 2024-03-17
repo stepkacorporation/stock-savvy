0673186ac384c12e9e16a95d150b6dd11f6f6862
Stepan Kornev <kornevstepan169@gmail.com>
2024-03-17 21:13:50 +0700

    [B] The API has been configured to receive stock data.
    - StockSerializer has been created.
    - StockViewSet has been created.
    - Routes for StockViewSet are configured.

--- backend/app/apps/stocks_api_v1/urls.py
+++ backend/app/apps/stocks_api_v1/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from .views import StockViewSet
+
+router = routers.SimpleRouter()
+router.register('stocks', StockViewSet)
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
--- backend/app/apps/stocks_api_v1/views.py
+++ backend/app/apps/stocks_api_v1/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Stock
+from .serializers import StockSerializer
+
+
+class StockViewSet(viewsets.ReadOnlyModelViewSet):
+    """A ViewSet for handling read-only operations on Stock instances."""
+
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
